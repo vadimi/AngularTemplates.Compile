2fe6e43bd2ef25de50b84dc63bdebe41112afff6
vadimi <vadim.ivanou@gmail.com>
2014-09-29 23:18:20 -0400

    Fix Mono build script

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 xbuild AngularTemplates.Compile/AngularTemplates.Compile.csproj /p:Configuration=Release /t:Clean
 xbuild AngularTemplates.Compile/AngularTemplates.Compile.csproj /p:Configuration=Release /t:Build
-xbuild AngularTemplates.Bundling/AngularTemplates.Bundling.csproj /p:Configuration=Release /t:Build
+xbuild AngularTemplates.Bundling/AngularTemplates.Bundling.csproj /p:Configuration=Release /t:Clean
 xbuild AngularTemplates.Bundling/AngularTemplates.Bundling.csproj /p:Configuration=Release /t:Build
 nuget Pack AngularTemplates.Compile.nuspec
