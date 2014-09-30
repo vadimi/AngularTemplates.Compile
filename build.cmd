7f1ca0dd80c0675b120ce6d90f37d16f2da8ac0a
vadimi <vadim.ivanou@gmail.com>
2014-09-29 23:13:52 -0400

    Updated build script and nuspec

--- build.cmd
+++ build.cmd
@@ -1,2 +1,3 @@
-msbuild AngularTemplates.Compile\AngularTemplates.Compile.csproj /p:Configuration=Release /t:Clean,Build
+msbuild AngularTemplates.Compile\AngularTemplates.Compile.csproj /p:Configuration=Release /t:Clean,Build /p:Platform="Any CPU" /p:OutputPath=bin/Release
+msbuild AngularTemplates.Bundling\AngularTemplates.Bundling.csproj /p:SolutionDir= /p:Configuration=Release /t:Clean,Build /p:Platform="Any CPU" /p:OutputPath=bin/Release
 nuget Pack AngularTemplates.Compile.nuspec
